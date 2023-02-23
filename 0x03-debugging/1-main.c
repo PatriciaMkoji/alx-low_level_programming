6368e67f9bd7a5c750842e11624459b4338b76b8
PatriciaMkoji <keyshotrisha@gmail.com>
2023-02-23 15:14:35 -0800

    1. Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop 
-* @i: has no increment / decrement argument
-* Return: 0
+* main - causes an infinite loop
+* Return: 0 error coz no increment / decrement value
 */
 
 int main(void)
@@ -15,9 +14,10 @@ int main(void)
 	i = 0;
 
 	while (i < 10)
-{
+	{
 		putchar(i);
-}
+	}
+
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
