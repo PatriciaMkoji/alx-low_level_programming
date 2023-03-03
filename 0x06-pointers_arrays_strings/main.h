283480f53ba1a69b9efa6dca9c5696373106ec53
PatriciaMkoji <keyshotrisha@gmail.com>
2023-03-02 17:56:37 -0800

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,6 +15,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
+char *cap_string(char *);
 
 #endif
