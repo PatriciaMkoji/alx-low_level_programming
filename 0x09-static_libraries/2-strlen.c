41897a93b5c37afef6c931de60923db9f638280d
PatriciaMkoji <keyshotrisha@gmail.com>
2023-03-14 11:49:59 -0700

    1. Without libraries what have we? We have no past and no future

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - retrns the length of a string
+ * @s: character to be used
+ * Return: nothing
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (*s++)
+		a++;
+	return (a);
+}
