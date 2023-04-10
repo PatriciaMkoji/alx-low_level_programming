206b7ab81dd83fa5393a03ec002a4269ae7b8a06
PatriciaMkoji <keyshotrisha@gmail.com>
2023-04-10 07:49:46 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,14 +21,10 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
-	{
-		text_content = "";
-	}
-	else
+	if (text_content != NULL)
 	{
 		fputs(text_content, createfile);
-		fclose(createfile);
 	}
+	fclose(createfile);
 	return (1);
 }
