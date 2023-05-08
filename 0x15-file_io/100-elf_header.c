8f1e59a7d01605c27d522c19d67af08ef8d1a508
PatriciaMkoji <keyshotrisha@gmail.com>
2023-05-08 14:38:38 -0700

    4. elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,12 +1,8 @@
 #include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <elf.h>
-#define ERRM "Error: %s is not an ELF file\n"
 /**
+ * main - displays the information contained in the ELF heade
  * @argc: number of arguments passed
  * @argv: an array of strings  passed to the program
- * main - displays the information contained in the ELF heade
  * Return: 0 on success, 98 on failure
  */
 int main(int argc, char **argv)
@@ -27,16 +23,15 @@ int main(int argc, char **argv)
 	}
 	if (read(fild, &header, sizeof(header)) != sizeof(header))
 	{
-        fprintf(stderr, ERRM, argv[1]);
-	close(fild);
-	return (98);
+		fprintf(stderr, ERRM, argv[1]);
+		close(fild);
+		return (98);
 	}
-	if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] != ELFMAG1 ||
-        header.e_ident[EI_MAG2] != ELFMAG2 || header.e_ident[EI_MAG3] != ELFMAG3)
+	if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] != ELFMAG1 || header.e_ident[EI_MAG2] != ELFMAG2 || header.e_ident[EI_MAG3] != ELFMAG3)
 	{
-        fprintf(stderr, ERRM, argv[1]);
-	close(fild);
-	return (98);
+		fprintf(stderr, ERRM, argv[1]);
+		close(fild);
+		return (98);
 	}
 	print_elf_header(header);
 	close(fild);
@@ -53,13 +48,15 @@ void print_elf_header(Elf64_Ehdr header)
 	printf("ELF Header:\n");
 	printf("  Magic:                             ");
 	for (indx = 0; indx < EI_NIDENT; indx++)
-        printf("%02x ", header.e_ident[indx]);
-	printf("\n");
-	printf("  Class:                             %s\n", header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
-	printf("  Data:                              %s\n", header.e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" : "2's complement, little endian");
-	printf("  Version:                           %d%s\n", header.e_ident[EI_VERSION], header.e_ident[EI_VERSION] == EV_CURRENT ? " (current)" : "");
-	printf("  OS/ABI:                            %d\n", header.e_ident[EI_OSABI]);
-	printf("  ABI Version:                       %d\n", header.e_ident[EI_ABIVERSION]);
-	printf("  Type:                              %d\n", header.e_type);
-	printf("  Entry point address:               %lx\n", header.e_entry);
+	{
+		printf("%02x ", header.e_ident[indx]);
+		printf("\n");
+		printf("  Class:                             %s\n", header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
+		printf("  Data:                              %s\n", header.e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" : "2's complement, little endian");
+		printf("  Version:                           %d%s\n", header.e_ident[EI_VERSION], header.e_ident[EI_VERSION] == EV_CURRENT ? " (current)" : "");
+		printf("  OS/ABI:                            %d\n", header.e_ident[EI_OSABI]);
+		printf("  ABI Version:                       %d\n", header.e_ident[EI_ABIVERSION]);
+		printf("  Type:                              %d\n", header.e_type);
+		printf("  Entry point address:               %lx\n", header.e_entry);
+	}
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,8 @@
 #include  <stdlib.h>
 #include  <string.h>
 
+#define ERRM "Error: %s is not an ELF file\n"
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
