0385acb61d326d53d28f458119c5895723991a28
PatriciaMkoji <keyshotrisha@gmail.com>
2023-05-09 02:15:47 -0700

    3.cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,68 +1,53 @@
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
+#include "main.h"
 
-#define BUFFER_SIZE 1024
+#define BUFFZ 1024
 
 /**
- * print_error - prints an error message to stderr and exits with a code
- * @message: the error message to print
- * @file: the file name to include in the error message
- * @code: the exit code to use when exiting
- *
- * Return: nothing
+ * print_error - it prints an error message to stderr
+ * @message: error message to be printed
+ * @file: its the file name to include in the err msg
+ * @code: exit code to used when exiting
+ * Return: no return
  */
 void print_error(const char *message, const char *file, int code)
 {
-    dprintf(STDERR_FILENO, message, file);
-    exit(code);
+	dprintf(STDERR_FILENO, message, file);
+	exit(code);
 }
-
 /**
- * main - copies the content of one file to another file
- * @argc: the number of arguments
- * @argv: an array of pointers to the arguments
- *
- * Return: 0 if successful, or an exit code if an error occurred
+ * main - it will copy the content of one file to another file
+ * @argc: number of arguments used
+ * @argv: array of pointers to arguments
+ * Return: 0 if successful, or an exit code
  */
 int main(int argc, char *argv[])
 {
-    char *file_from, *file_to;
-    int fd_from, fd_to;
-    char buffer[BUFFER_SIZE];
-    ssize_t nread, nwritten;
+	char *file_from, *file_to;
+	int fild_from, fild_to;
+	char buffer[BUFFZ];
+	ssize_t readn, writn;
 
-    if (argc != 3)
-        print_error("Usage: %s file_from file_to\n", argv[0], 97);
-
-    file_from = argv[1];
-    file_to = argv[2];
-
-    fd_from = open(file_from, O_RDONLY);
-    if (fd_from < 0)
-        print_error("Error: Can't read from file %s\n", file_from, 98);
-
-    fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (fd_to < 0)
-        print_error("Error: Can't write to %s\n", file_to, 99);
-
-    while ((nread = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-    {
-        nwritten = write(fd_to, buffer, nread);
-        if (nwritten != nread)
-            print_error("Error: Can't write to %s\n", file_to, 99);
-    }
-    if (nread < 0)
-        print_error("Error: Can't read from file %s\n", file_from, 98);
-
-    if (close(fd_from) < 0)
-        print_error("Error: Can't close fd %d\n", file_from, 100);
-
-    if (close(fd_to) < 0)
-        print_error("Error: Can't close fd %d\n", file_to, 100);
-
-    return (0);
+	if (argc != 3)
+		print_error("Usage: %s file_from file_to\n", argv[0], 97);
+	file_from = argv[1];
+	file_to = argv[2];
+	fild_from = open(file_from, O_RDONLY);
+	if (fild_from < 0)
+		print_error("Error: Can't read from file %s\n", file_from, 98);
+	fild_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fild_to < 0)
+		print_error("Error: Can't write to %s\n", file_to, 99);
+	while ((readn = read(fild_from, buffer, BUFFZ)) > 0)
+	{
+		writn = write(fild_to, buffer, readn);
+		if (writn != readn)
+			print_error("Error: Can't write to %s\n", file_to, 99);
+	}
+	if (readn < 0)
+		print_error("Error: Can't read from file %s\n", file_from, 98);
+	if (close(fild_from) < 0)
+		print_error("Error: Can't close fd %d\n", file_from, 100);
+	if (close(fild_to) < 0)
+		print_error("Error: Can't close fd %d\n", file_to, 100);
+	return (0);
 }
-
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include  <stdlib.h>
 #include  <string.h>
+#include <unistd.h>
+#include <errno.h>
 
 #define ERRM "Error: %s is not an ELF file\n"
 
@@ -14,5 +16,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void print_elf_header(Elf64_Ehdr header);
+void print_error(const char *message, const char *file, int code);
 
 #endif
