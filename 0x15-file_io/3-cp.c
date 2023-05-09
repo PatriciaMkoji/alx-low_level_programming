6d18946e2f7a8eff8d2ec6b2bc62e16980c28d74
PatriciaMkoji <keyshotrisha@gmail.com>
2023-05-09 02:47:31 -0700

    3.cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,7 +23,7 @@ void print_error(const char *message, const char *file, int code)
 int main(int argc, char *argv[])
 {
 	char *file_from, *file_to;
-	int fild_from, fild_to;
+	int fd_from, fd_to;
 	char buffer[BUFFZ];
 	ssize_t readn, writn;
 
@@ -31,13 +31,13 @@ int main(int argc, char *argv[])
 		print_error("Usage: %s file_from file_to\n", argv[0], 97);
 	file_from = argv[1];
 	file_to = argv[2];
-	fild_from = open(file_from, O_RDONLY);
-	if (fild_from < 0)
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from < 0)
 		print_error("Error: Can't read from file %s\n", file_from, 98);
-	fild_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fild_to < 0)
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd_to < 0)
 		print_error("Error: Can't write to %s\n", file_to, 99);
-	while ((readn = read(fild_from, buffer, BUFFZ)) > 0)
+	while ((readn = read(fd_from, buffer, BUFFZ)) > 0)
 	{
 		writn = write(fild_to, buffer, readn);
 		if (writn != readn)
@@ -45,9 +45,9 @@ int main(int argc, char *argv[])
 	}
 	if (readn < 0)
 		print_error("Error: Can't read from file %s\n", file_from, 98);
-	if (close(fild_from) < 0)
+	if (close(fd_from) < 0)
 		print_error("Error: Can't close fd %d\n", file_from, 100);
-	if (close(fild_to) < 0)
+	if (close(fd_to) < 0)
 		print_error("Error: Can't close fd %d\n", file_to, 100);
 	return (0);
 }
